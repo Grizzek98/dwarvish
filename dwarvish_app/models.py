a3ba8ccc1d9efada8346048e7cda964ae1bd8131
andben98 <benjaminanderson98@live.com>
2022-02-15 15:55:24 -0700

    update

--- dwarvish_app/models.py
+++ dwarvish_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Lesson(models.model):
+    title = models.DateField()
+    desciption = models.CharField()
--- dwarvish_app/views.py
+++ dwarvish_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from dwarvish_app.models import Lesson
 
-# Create your views here.
+def lesson_index(request):
+    lessons = Lesson.all().order_by('-title')
+    context = {'lessons': lessons}
+    return render(request, 'lesson_index.html', context)
+
+def lesson_detail(request, pk):
+    lesson = Lesson.objects.get(pk=pk)
+    context = {'lesson': lesson}
+    return render(request, 'lesson_detail.html', context)
