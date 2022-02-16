c73d2144d0547b23cfed9ba87ad52f6ac4e4bd7d
Stewart Rowley <78276704+stewartrowley@users.noreply.github.com>
2022-02-16 14:19:20 -0700

    update

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,6 +1,9 @@
+
+from operator import index
 from django.urls import path
 from hello_world import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world')
+    path('', views.hello_world, name='hello_world'),
+    path('lesson1/index.html', views.index, name='lesson1')
 ]
