656f709a66f4b680a825bbfa30f53cf68cc9763d
andben98 <benjaminanderson98@live.com>
2022-03-09 19:20:18 -0700

    changed dwarvish max_length to 6, registered on admin

--- dwarvish_app/models.py
+++ dwarvish_app/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Char(models.Model):
     english = models.CharField(max_length=3)
-    dwarvish = models.CharField(max_length=1)
+    dwarvish = models.CharField(max_length=6)
