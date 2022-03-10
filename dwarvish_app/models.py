7f6db7924c67cf9d235fbd984dc4a81b3d1ea0f8
Benjamin Anderson <benjaminanderson98@live.com>
2022-03-10 12:43:37 -0700

    added sqlite_test.py which interfaces directly with the db. added chars to db

--- dwarvish_app/models.py
+++ dwarvish_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Char(models.Model):
-    english = models.CharField(max_length=3)
-    dwarvish = models.CharField(max_length=6)
+    english = models.CharField(max_length=10)
+    dwarvish = models.CharField(max_length=1)
