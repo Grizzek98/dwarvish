7a9490d5115269d707207ed2d18d488be3f59200
artable <r.jonah.deal@gmail.com>
2022-03-10 20:12:54 -0700

    implement API incl URL for full char model

--- dwarvish_app/views.py
+++ dwarvish_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Char
+from dwarvish_app import serializers
 
 def lesson_index(request):
     return render(request, 'lesson_index.html', {})
 
-
 def index(request):
     return render(request, 'lesson1/index.html', {})
+
+class CharAll(viewsets.ModelViewSet) :
+    serializer_class = serializers.EngCirthSerializer
+    queryset = Char.objects.all()
